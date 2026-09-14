5e2d5f05a8d6f4f2458af08e3b38927950c26dce
Ashi <nightlyshaded@gmail.com>
2026-09-14 13:45:51 +0200

    01-Get - Task 1, 2

---
+++ consolegameig/CatFact.cs
@@ -0,0 +1,7 @@
+﻿namespace consolegameig;
+
+public class CatFact
+{
+    public string Fact { get; set; } = "";
+    public int Length { get; set; }
+}
---
+++ consolegameig/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using consolegameig;
+
+HttpClient client = new HttpClient();
+
+CatFact? catFact = await client.GetFromJsonAsync<CatFact>( "https://catfact.ninja/fact");
+
+Console.WriteLine(catFact.Fact);
+Console.WriteLine(catFact.Fact);
+
